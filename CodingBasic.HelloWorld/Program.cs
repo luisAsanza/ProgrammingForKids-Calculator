d56a7602dd2999b4ea5f7810e60fcda72ff860b8
luis.asanza <luis.asanza@verndale.com>
2022-06-25 15:35:48 -0500

    Add project files.

---
+++ CodingBasic.HelloWorld/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CodingBasic.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arrayTest = new int[] { 2, 1, 5, 5, 6, 3, 7, 4, 2, 4, 6, 2, 3, 99, 8 };
+
+            Console.WriteLine($"This array has {arrayTest.Length} elements.");
+            Console.WriteLine();
+
+            //Console.WriteLine(arrayTest[0]);
+            //Console.WriteLine(arrayTest[1]);
+            //Console.WriteLine(arrayTest[2]);
+
+            for(int i = 0; i < arrayTest.Length; i++)
+            {
+                Console.WriteLine($"arrayTest[{i}] = {arrayTest[i]}");
+            }
+
+
+
+
+            //Console.WriteLine("SUM Calculator program");
+            //Console.WriteLine();
+            //Console.WriteLine("Insert a:");
+            //string a = Console.ReadLine();
+            //Console.WriteLine("Insert b:");
+            //string b = Console.ReadLine();
+
+            //Console.WriteLine($"The values inserted are: {a} and {b}");
+
+            //int aInt = int.Parse(a);
+            //int bInt = int.Parse(b);
+            //int result = aInt + bInt;
+
+            //Console.WriteLine($"The sum of {aInt} + {bInt} = {result}.");
+        }
+    }
+}
---
+++ CondigBasic.Calculator/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace CondigBasic.Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print program name
+            Console.WriteLine("Calculator Machine");
+            //Print the menu
+            Console.WriteLine("a:Sum");
+            Console.WriteLine("b:Subtraction");
+            Console.WriteLine("c:Division");
+            Console.WriteLine("d:Multiplication");
+            Console.WriteLine();
+
+            //Select one option
+            //x
+            string option = Console.ReadLine();
+
+            /*
+             option == "a"
+            option == "b"
+             option == "c"
+            option == "d"
+
+            Si "limpias tu cuarto" O "bañas al perro" podrás ir al cine
+            Si "limpias tu cuarto" Y "bañas al perro" podrás ir al cine
+
+            O = OR
+            Y = AND
+
+             */
+
+            if (option == "a" || option == "b" || option == "c" || option == "d")
+            {
+                Console.WriteLine($" Insert a value x: ");
+                string x = Console.ReadLine();
+                Console.WriteLine($" Insert a value z: ");
+                string z = Console.ReadLine();
+
+                //if the user pick the a option them sum
+                if (option == "a")
+                {
+                    int xInt;
+                    int zInt;
+                    bool resultX = int.TryParse(x, out xInt);
+                    bool resultZ = int.TryParse(z, out zInt);
+
+                    /*
+                     Si el parsing de x fue exitoso, entonces resultX será true
+                    Si el parsing de z fue exitoso, entonces resultZ será true
+
+                    "3"         > resultX = true
+                    "ssss"      > resultZ = false
+                     */
+
+                    if (resultX && resultZ)
+                    {
+                        int resultSum = xInt + zInt;
+                        Console.WriteLine($"{xInt} + {zInt} = {resultSum}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid format!");
+                    }
+
+                    /*
+                        x   Z   result
+                        ================
+                        1   ss  false
+                        ss  1   false
+                        ss  ss  false
+                        1   1   true    if
+                     */
+                }
+                else if (option == "b")
+                {
+                    int aInt = int.Parse(x);
+                    int bInt = int.Parse(z);
+                    int result = aInt - bInt;
+                    Console.WriteLine($"{aInt} - {bInt} = {result}");
+                }
+                else if (option == "c")
+                {
+                    decimal aDecimal = decimal.Parse(x);
+                    decimal bDecimal = decimal.Parse(z);
+                    decimal result = aDecimal / bDecimal;
+                    Console.WriteLine($"{aDecimal} / {bDecimal} = {result}");
+                }
+                else if (option == "d")
+                {
+                    int aInt = int.Parse(x);
+                    int bInt = int.Parse(z);
+                    int result = aInt * bInt;
+                    Console.WriteLine($"{aInt} x {bInt} = {result}");
+                }
+                else
+                {
+                    Console.WriteLine($"invalid option");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid option.");
+            }
+
+        }
+    }
+}
