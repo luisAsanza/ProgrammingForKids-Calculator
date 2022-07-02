89868a761b17b63881ebdbfcc1c3b508615752a4
luis.asanza <luis.asanza@verndale.com>
2022-07-02 17:35:26 -0500

    Create new project "IterativeStatements"

---
+++ CodingBasic.IterativeStatements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CodingBasic.IterativeStatements
+{
+    /// <summary>
+    /// Iterative statements
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///1. (Luis) Print from 1 to 100
+            //for(int i = 1; i <= 100; i = i + 1)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            ///2. (Elian) Print odd numbers from 1 to 100
+            
+
+
+            ///3. (Alan) Print the numbers 10 by 10 up to 100
+
+
+        }
+    }
+}
